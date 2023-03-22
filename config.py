5933eccfb035bcc17227dd9e9fc7a9dead5b48c1
prknezek <prknezek@gmail.com>
2023-03-22 00:10:14 -0500

    Player Movement

--- config.py
+++ config.py
@@ -3,8 +3,18 @@ WIN_HEIGHT = 720
 TILESIZE = 16
 FPS = 60
 
+# PLAYER
+PLAYER_SPEED = 3
+
 # LAYERS (higher number means higher priority)
 PLAYER_LAYER = 1
 
+# COLORS
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
+
+# FACING
+FACING_LEFT = 'left'
+FACING_UP = 'up'
+FACING_RIGHT = 'right'
+FACING_DOWN = 'down'
