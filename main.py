4495caf98282d524114dec0556b27d4e04fd90d4
prknezek <prknezek@gmail.com>
2023-03-21 22:59:52 -0500

    Moving Red Circle

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pygame
 
 pygame.init()
 FRAMES = 60
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1024, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -14,10 +17,22 @@ while running:
             running = False
 
         # fill screen with a color to wipe away anything from last frame
-        screen.fill("blue")
+    screen.fill("blue")
 
-        pygame.display.flip()
+    pygame.draw.circle(screen, "red", player_pos, 40)
 
-        clock.tick(FRAMES)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] :
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s] :
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a] :
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d] :
+        player_pos.x += 300 * dt
+
+    pygame.display.flip()
+
+    dt = clock.tick(FRAMES) / 1000
 
 pygame.quit()
