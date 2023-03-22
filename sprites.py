c2fab7d14506689b96361055446da009057d80f3
prknezek <prknezek@gmail.com>
2023-03-22 09:26:27 -0500

    small movement simplification

--- sprites.py
+++ sprites.py
@@ -15,8 +15,6 @@ class Player(pygame.sprite.Sprite) :
         self.width = TILESIZE
         self.height = TILESIZE
 
-        self.x_change = 0
-        self.y_change = 0
         self.dt = clock.tick(FPS) / 1000
 
         self.facing = FACING_DOWN
@@ -34,26 +32,20 @@ class Player(pygame.sprite.Sprite) :
         pass
         self.movement()
 
-        self.rect.x += self.x_change
-        self.rect.y += self.y_change
-
-        self.x_change = 0
-        self.y_change = 0
-
     def movement(self) :
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT] or keys[pygame.K_a] :
-            self.x_change -= PLAYER_SPEED * self.dt
+            self.rect.x -= PLAYER_SPEED * self.dt
             self.facing = FACING_LEFT
         if keys[pygame.K_RIGHT] or keys[pygame.K_d] :
-            self.x_change += PLAYER_SPEED * self.dt
+            self.rect.x += PLAYER_SPEED * self.dt
             self.facing = FACING_RIGHT
         if keys[pygame.K_UP] or keys[pygame.K_w] :
-            self.y_change -= PLAYER_SPEED * self.dt
+            self.rect.y -= PLAYER_SPEED * self.dt
             self.facing = FACING_UP
         if keys[pygame.K_DOWN] or keys[pygame.K_s] :
-            self.y_change += PLAYER_SPEED * self.dt
+            self.rect.y += PLAYER_SPEED * self.dt
             self.facing = FACING_DOWN
 
 class Tile(pygame.sprite.Sprite) :
