899beab9efb7e5ffeb73ce7183566fb456d3172b
prknezek <prknezek@gmail.com>
2023-03-25 12:40:39 -0500

    Initialize CameraGroup

--- code/level.py
+++ code/level.py
@@ -8,7 +8,7 @@ class Level :
         self.display_surface = pygame.display.get_surface()
 
         # sprite groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = CameraGroup()
 
         self.setup()
 
@@ -17,5 +17,16 @@ class Level :
 
     def run(self, dt) :
         self.display_surface.fill('black')
-        self.all_sprites.draw(self.display_surface)
+        #self.all_sprites.draw(self.display_surface)
+        self.all_sprites.custom_draw()
         self.all_sprites.update(dt)
+
+class CameraGroup(pygame.sprite.Group) :
+    def __init__(self) :
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def custom_draw(self) :
+        for sprite in self.sprites() :
+            self.display_surface.blit(sprite.image, sprite.rect)
+    
