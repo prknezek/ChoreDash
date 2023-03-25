0cb6fbb3a9c9d6e0a721b38b061e71496d1691ec
prknezek <prknezek@gmail.com>
2023-03-25 13:35:28 -0500

    support.py changes

--- code/support.py
+++ code/support.py
@@ -1,5 +1,3 @@
-from csv import reader
-import config as cg
 from os import walk
 import pygame
 
@@ -14,29 +12,3 @@ def import_folder(path) :
             surface_list.append(image_surface)
     
     return surface_list
-
-def import_csv_layout(path) :
-    tile_map = []
-    with open(path) as map :
-        # map is csv data
-        level = reader(map, delimiter = ',')
-        for row in level :
-            tile_map.append(list(row))
-        return tile_map
-
-def import_cut_graphics(path) :
-    surface = pygame.image.load(path).convert_alpha()
-    tile_num_x = int(surface.get_size()[0] / cg.TILESIZE)
-    tile_num_y = int(surface.get_size()[1] / cg.TILESIZE)
-
-    cut_tiles = []
-    for row in range(tile_num_y) :
-        for col in range(tile_num_x) :
-            x = col * cg.TILESIZE
-            y = row * cg.TILESIZE
-
-            new_surface = pygame.Surface((cg.TILESIZE, cg.TILESIZE), pygame.SRCALPHA)
-            new_surface.blit(surface, (0, 0), pygame.Rect(x, y, cg.TILESIZE, cg.TILESIZE))
-            cut_tiles.append(new_surface)
-
-    return cut_tiles
