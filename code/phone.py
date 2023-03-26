ad4873504b4596f358ca8ccfdab13265338fa6e3
adnanysf <asubyusuf@gmail.com>
2023-03-26 13:15:49 -0500

    Phone with timer

--- code/phone.py
+++ code/phone.py
@@ -1,7 +1,6 @@
 import pygame
 import sys
 import config as cg
-#from settings import *
 
 class Phone:
     def __init__(self):
@@ -10,11 +9,35 @@ class Phone:
         self.font = pygame.font.Font('graphics/Pixellari.ttf', 30)
         self.phone_image = pygame.image.load('graphics/phone/Phone.png').convert_alpha()
         self.phone_image.set_colorkey((0, 0, 0))
-
+        self.last_time = 0
+        self.minutes = 3
+        self.seconds = 0
+        self.countdown_time = 3 * 60 
+        
     def display(self, screen):
         self.screen.blit(self.phone_image, (0, 0))
+
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_time >= 1000: 
+            self.last_time = current_time
+            self.seconds -= 1
+            if self.seconds < 0:
+                self.seconds = 59
+                self.minutes -= 1
+                if self.minutes < 0:
+                    self.minutes = 0
+                    self.seconds = 0
+
+        if self.countdown_time > 0:
+            time_str = f"{self.minutes:02d}:{self.seconds:02d}"
+            time_text = self.font.render(time_str, True, (255, 255, 255))
+            self.screen.blit(time_text, (20, 20))
+        else:
+            time_text = self.font.render("TIME'S UP!", True, (255, 0, 0))
+            self.screen.blit(time_text, (10, 20))
+
         screen.blit(self.screen, (50, cg.SCREEN_HEIGHT//2)) 
-        pygame.display.flip() # Update the display
+        pygame.display.flip() 
 
     def run(self, screen):
         if self.show_phone:
