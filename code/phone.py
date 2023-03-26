9d921e58bd0228089b715c3223d56c58d2455ff8
adnanysf <asubyusuf@gmail.com>
2023-03-26 00:01:57 -0500

    added phone display

--- code/phone.py
+++ code/phone.py
@@ -6,15 +6,15 @@ import config as cg
 class Phone:
     def __init__(self):
         self.show_phone = False
-        self.screen = pygame.Surface((cg.SCREEN_WIDTH//4, cg.SCREEN_HEIGHT//2))
+        self.screen = pygame.Surface((cg.SCREEN_WIDTH//4, cg.SCREEN_HEIGHT//2), pygame.SRCALPHA) 
         self.font = pygame.font.Font('graphics/Pixellari.ttf', 30)
+        self.phone_image = pygame.image.load('graphics/phone/Phone.png').convert_alpha()
+        self.phone_image.set_colorkey((0, 0, 0))
 
     def display(self, screen):
-        red = (255, 0, 0)
-        rect = pygame.Rect(0, 0, cg.SCREEN_WIDTH//4, cg.SCREEN_HEIGHT//2)
-        pygame.draw.rect(self.screen, red, rect) 
+        self.screen.blit(self.phone_image, (0, 0))
         screen.blit(self.screen, (50, cg.SCREEN_HEIGHT//2)) 
-        pygame.display.flip() # Update the display  
+        pygame.display.flip() # Update the display
 
     def run(self, screen):
         if self.show_phone:
