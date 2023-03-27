7943a55e58f79ca55add35be259a0f747e74a326
prknezek <prknezek@gmail.com>
2023-03-27 13:34:10 -0500

    fixed map

--- house/tilesets/10_Birthday_party_Shadowless_32x32.tsx
+++ house/tilesets/10_Birthday_party_Shadowless_32x32.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="10_Birthday_party_Shadowless_32x32" tilewidth="32" tileheight="32" tilecount="84" columns="12">
- <image source="../../graphics/tiles/10_Birthday_party_Shadowless_32x32.png" width="384" height="224"/>
+ <image source="../../graphics/tiles/assets/10_Birthday_party_Shadowless_32x32.png" width="384" height="224"/>
 </tileset>
--- house/tilesets/4_Bedroom_32x32.tsx
+++ house/tilesets/4_Bedroom_32x32.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="4_Bedroom_32x32" tilewidth="32" tileheight="32" tilecount="1712" columns="16">
- <image source="../../graphics/tiles/4_Bedroom_Shadowless_32x32.png" width="512" height="3424"/>
+ <image source="../../graphics/tiles/assets/4_Bedroom_Shadowless_32x32.png" width="512" height="3424"/>
 </tileset>
