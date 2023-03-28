f9b05cbc3476f7c09c474ff53d975088da7d5be1
Hemanth <hmukesh@outlook.com>
2023-03-28 10:38:00 -0500

    phone MVP

--- code/phone.py
+++ code/phone.py
@@ -46,18 +46,18 @@ class Phone:
     def __init__(self):
         
         # options
-        self.width = 108 # 135
+        self.width = 110 # 135
         self.height = 196 # 245
 
         self.show_phone = True
         self.phone_surf = pygame.Surface((self.width, self.height), pygame.SRCALPHA) 
-        self.font = pygame.font.Font('graphics/Pixeltype.ttf', 15)
+        self.font = pygame.font.Font('graphics/5x5.ttf', 10)
         #self.font = pygame.font.SysFont(None, 15)
-        self.phone_image = pygame.transform.scale(pygame.image.load('graphics/phone/phonesprite-1.png').convert_alpha(), (self.width, self.height))
+        self.phone_image = pygame.transform.scale(pygame.image.load('graphics/phone/phonesprite-2.png').convert_alpha(), (self.width, self.height))
         self.phone_image.set_colorkey((0, 0, 0))       
 
-        self.space = 23 # space between texts
-        self.padding = 6 # padding in text bubble
+        self.space = 10 # space between texts
+        self.padding = 1 # padding in text bubble
         self.leftrightspace = 7 # space on left and right side of texts
 
         self.left_coord = 30
@@ -71,9 +71,9 @@ class Phone:
         print(self.phonescreen_rect.width, self.phonescreen_rect.height)
 
         self.texts = [
-            ("omw home sweetie, be sure u did all the things on the fridge !", "5:10"),
-            ("almost home ! ", "5:28"),
-            ("note: press tab to close the phone and start the game! ", "5:28")
+            ("almost home, do your chores !", "5:10"),
+            ("check the fridge list ! ", "5:28"),
+            ("press tab to start ! ", "5:28")
         ]
         self.initTexts()
 
@@ -84,30 +84,51 @@ class Phone:
         self.countdown_time = 3 * 60 
     
     def initTexts(self):
-        self.totalHeight = 0
-        self.text_surfs = []
-        for item in self.texts:            
-            for line in wrapline(item[0], self.font, self.phonescreen_rect.width - 20):
+        self.totalHeight = 0        
+        self.all_texts = []
+        for item in self.texts:
+            self.text_surfs = []
+            for line in wrapline(item[0], self.font, 80):
                 text_surf = self.font.render(line, True, 'Black')
-                self.text_surfs.append(text_surf)        
+                self.text_surfs.append(text_surf)
+            self.all_texts.append(self.text_surfs)
         #self.text_surfs.reverse()
 
     def showText(self, text_surf, top):
         # white background
-        bg_rect = pygame.Rect(self.phonescreen_rect.left + 5,
-                              self.phonescreen_rect.top + top + 30,
-                              self.phonescreen_rect.width - 20,
-                              text_surf.get_height() + (self.padding * 2))
-        pygame.draw.rect(self.phone_surf, 'White', bg_rect, 0, 4)
+        # bg_rect = pygame.Rect(self.phonescreen_rect.left + 5,
+        #                       self.phonescreen_rect.top + top + 30,
+        #                       self.phonescreen_rect.width - 20,
+        #                       text_surf.get_height() + (self.padding * 2))
+        # pygame.draw.rect(self.phone_surf, 'White', bg_rect, 0, 4)
 
         # show text surf
-        text_surf_rect = text_surf.get_rect(midleft = (bg_rect.left + 3, bg_rect.midleft[1]))
+        text_surf_rect = text_surf.get_rect(midleft = (15, 31+top))
         self.phone_surf.blit(text_surf, text_surf_rect)
 
     def display(self, display_surf):
         #timer
         # display_surf.blit(self.phone_image, (0, 0))
 
+        self.phone_rect = self.phone_surf.get_rect(bottom = cg.SCREEN_HEIGHT, left = self.left_coord)
+
+        # render phone
+        self.phone_surf.blit(self.phone_image, (0, 0))        
+
+        # render texts
+        topOffset = 0
+        for text in self.all_texts:
+            for text_surf in text:
+                # text_surf_rect = text_surf.get_rect(left = 15, bottom = self.height - (text_surf_index * 10))
+                # self.phone_surf.blit(text_surf, text_surf_rect)
+                self.showText(text_surf, topOffset)
+                topOffset += self.space
+            topOffset += 18
+        
+        # contact_text = self.font.render("Mom", True, 'White')
+        # contact_text_rect = contact_text.get_rect(midbottom = (self.phonescreen_rect.center[0], self.phonescreen_rect.top + 17))
+        #self.phone_surf.blit(contact_text, contact_text_rect)
+        
         current_time = pygame.time.get_ticks()
         if current_time - self.last_time >= 1000: 
             self.last_time = current_time
@@ -121,32 +142,17 @@ class Phone:
 
         if self.countdown_time > 0:
             time_str = f"{self.minutes:02d}:{self.seconds:02d}"
-            time_text = self.font.render(time_str, True, (255, 255, 255))
-            display_surf.blit(time_text, (20, 20))
-                
+            time_text = self.font.render("ETA            " + time_str, True, (255, 255, 255))
+            time_text_rect = time_text.get_rect(left = 20, bottom = self.phone_rect.height-32)
+            self.phone_surf.blit(time_text, time_text_rect)
 
-        # # render blue background and top bar
-        # pygame.draw.rect(self.phone_surf, (139,170,220), self.phonescreen_rect)
-        # pygame.draw.rect(self.phone_surf, (71, 71, 71), pygame.Rect(self.phonescreen_rect.left,self.phonescreen_rect.top,self.phonescreen_rect.width, 20))
-
-        # render phone border
-        self.phone_surf.blit(self.phone_image, (0, 0))        
-
-        # # render texts
-        # topOffset = 0
-        # for text_surf_index, text_surf in enumerate(self.text_surfs):
-        #     # text_surf_rect = text_surf.get_rect(left = 15, bottom = self.height - (text_surf_index * 10))
-        #     # self.phone_surf.blit(text_surf, text_surf_rect)
-        #     self.showText(text_surf, topOffset)
-        #     topOffset += text_surf.get_height() + self.space
-        
-        # contact_text = self.font.render("Mom", True, 'White')
-        # contact_text_rect = contact_text.get_rect(midbottom = (self.phonescreen_rect.center[0], self.phonescreen_rect.top + 17))
-        #self.phone_surf.blit(contact_text, contact_text_rect)
-        
-        # draw phone to actual display
-        self.phone_rect = self.phone_surf.get_rect(bottom = cg.SCREEN_HEIGHT, left = self.left_coord)
+        # draw phone to actual display        
         display_surf.blit(self.phone_surf, self.phone_rect)
+
+        # random vacuum text testing
+        # testing_surf = self.font.render("VACUUM EQUIPPED", False, 'White')
+        # testing_surf_rect = testing_surf.get_rect(center = (cg.SCREEN_WIDTH/2, cg.SCREEN_HEIGHT/2 + 60))
+        # display_surf.blit(testing_surf, testing_surf_rect)
         
         pygame.display.flip() # Update the display
 
